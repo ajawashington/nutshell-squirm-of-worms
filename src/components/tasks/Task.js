a43f498c16ae316ed43b987448f341fd3578a58b
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-01-31 13:07:18 -0600

    added messageProvider

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -5,11 +5,12 @@ import { TaskContext } from "./TaskProvider";
 export default ({ task, history }) => {
     const { deleteTask } = useContext(TaskContext)
     const [hideText, setHideText] = useState(true);
+
 //write a function that returns either the string hidden or a blank string based on state
 //write a function that returns the entire component, or just the unhidden part, based on state 
 
-    const hideTask = () => {
-        if (hideText === true){
+    // const hideTask = () => {
+    //     if (hideText === true){
             
             return(
                 
@@ -17,13 +18,13 @@ export default ({ task, history }) => {
         
                 <input type="checkbox" className="" id={ `task--${task.id}` }
         
-                    onClick={
-                    () => {
-                        setHideText(false)
-                        .then(() => {
-                            history.push("/")            
-                        })
-                    }}
+                    // onClick={
+                    // () => {
+                    //     setHideText(false)
+                    //     .then(() => {
+                    //         history.push("/")            
+                    //     })
+                    // }}
                 />
         
                 <div className="task__text">{ task.text }</div>
@@ -41,11 +42,11 @@ export default ({ task, history }) => {
             </section>
         
         )
-        } else {
-            return("")
-        }
+    //     } else {
+    //         return("")
+    //     }
         
-    }
-    hideTask()
+    // }
+    // hideTask()
 
 }
