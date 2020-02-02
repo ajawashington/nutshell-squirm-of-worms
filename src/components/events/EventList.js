c2539483b899c44ae46baa918b8afac83354a850
namitamanohar <manohar.namita@gmail.com>
2020-02-01 18:02:45 -0600

    events and articles only show activer users and their friends

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -2,9 +2,36 @@ import React, { useContext } from "react"
 import { EventContext } from "./EventProvider"
 import Event from "./Event"
 import "./Events.css"
+import { FriendContext } from "../friends/FriendProvider"
 
 export default (props) => {
     const { events } = useContext(EventContext)
+    const { friends } = useContext(FriendContext)
+
+    const eventsArray = []
+
+    const activeUserEvents = events.filter(e => {
+        return e.userId === parseInt(localStorage.getItem("nutshell_user"),10)
+    })
+    activeUserEvents.map(a => {
+        return eventsArray.push(a)
+    })
+
+    const activeUserFriendsArray = friends.filter( f => {
+        return f.activeUserId === parseInt(localStorage.getItem("nutshell_user"), 10)
+    })
+    
+   
+    const friendEvents = activeUserFriendsArray.map( f => {
+        return events.filter(e => e.userId === f.userId)
+    })
+    
+    const singeleFriendEvent = friendEvents.map(f => {
+        return f.map(sf => {return eventsArray.push(sf)}
+    )} )
+   
+
+    console.log("active user + friends'event array", eventsArray)
 
     return (
         <>
@@ -12,10 +39,11 @@ export default (props) => {
             <div className="events">
 
                 {
-                    events.map(e => {
+                    eventsArray.map(e => {
                         return <Event key={e.id} event={e} {...props} />
                     })
                 }
+           
             </div>
         </>
     )
